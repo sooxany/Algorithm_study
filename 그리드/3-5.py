f143f63ee37f42b0dc535dc400508cc587cf4ab0
sooxany <soojin0929@naver.com>
2023-04-09 15:55:58 +0900

    dd

--- 그리드/3-5.py
+++ 그리드/3-5.py
@@ -1,9 +1,11 @@
+#1이 될 때까지
+
 n, k = map(int, input().split())
 
 cnt = 0
 
-while True:
-    if (n==1):
+while True: # 반복
+    if (n==1): # 우리가 원하는 답이 나오면 반복문 탈출
         break
     elif (n % k) == 0:
         n //= k
