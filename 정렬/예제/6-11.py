d43e3543f4a5cef890c77bc2fc30020ae5a22ec9
sooxany <soojin0929@naver.com>
2023-05-07 17:50:41 +0900

    page180 실전문제 '성적이 낮은 순서로 학생 출력하기'

---
+++ 정렬/예제/6-11.py
@@ -0,0 +1,18 @@
+num = int(input())
+
+array = []
+
+for i in range(num):
+    input_data = input().split()
+    array.append((input_data[0], int(input_data[1])))
+
+array = sorted(array, key=lambda student: student[1])
+
+for student in array:
+    print(student[0], end=' ')
+
+
+
+
+
+
